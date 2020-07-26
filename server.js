1870da5b4bb71656a38cd592f4102ab0adc92b46
Joe Haefling <josephhaefling@gmail.com>
2020-07-25 18:55:41 -0600

    Add Delete favorite functionality

--- server.js
+++ server.js
@@ -17,44 +17,7 @@ app.get('/', (request, response) => {
   response.send('ArtisTry Backend');
 });
 
-app.locals.favorites = [
-  {
-        "title": "Mona Lisa",
-        "contentId": 225189,
-        "artistContentId": 225091,
-        "artistName": "Leonardo da Vinci",
-        "completitionYear": 1519,
-        "yearAsString": "1519",
-        "width": 2835,
-        "image": "https://uploads7.wikiart.org/images/leonardo-da-vinci/mona-lisa.jpg!Large.jpg",
-        "height": 4289,
-        "name": 'image'
-    },
-  {
-      "title": "The River Thames with St. Paul's Cathedral on Lord Mayor's Day",
-      "contentId": 250550,
-      "artistContentId": 250406,
-      "artistName": "Canaletto",
-      "completitionYear": 1746,
-      "yearAsString": "1746",
-      "width": 1296,
-      "image": "https://uploads6.wikiart.org/images/canaletto/the-river-thames-with-st-paul-s-cathedral-on-lord-mayor-s-day.jpg!Large.jpg",
-      "height": 676,
-      "name": 'image'
-    },
-    {
-      "title": "Just what is it that makes today's homes so different, so appealing?",
-      "contentId": 243774,
-      "artistContentId": 243771,
-      "artistName": "Richard Hamilton",
-      "completitionYear": 1956,
-      "yearAsString": "1956",
-      "width": 1211,
-      "image": "https://uploads3.wikiart.org/images/richard-hamilton/http-en-wikipedia-org-wiki-file-hamilton-appealing2-jpg-1956.jpg!Large.jpg",
-      "height": 1260,
-      "name": 'image'
-    },
-  ]
+app.locals.favorites = []
 
 
 app.get('/api/v1/favorites', (request, response) => {
