1402b0fb46fd74f8074b3bb1eaf47147de7b7fb2
Charles Wefso <cwefso@gmail.com>
2020-07-28 14:06:58 -0600

    prepares app for deployment

---
+++ build/precache-manifest.1fe783e5da042ea16bd2201fef5d97e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca769b2a2a330bb6b87333fdb9a73b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34048c17d9e78d9a3503",
+    "url": "/static/css/main.19fd192a.chunk.css"
+  },
+  {
+    "revision": "9b0c4dc8d117d08bf1bc",
+    "url": "/static/js/2.d49909c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d49909c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34048c17d9e78d9a3503",
+    "url": "/static/js/main.35dc3e06.chunk.js"
+  },
+  {
+    "revision": "8d316f54d3930c84f6b8",
+    "url": "/static/js/runtime-main.a92c1044.js"
+  },
+  {
+    "revision": "d5ef1ff06b98c9c90e269ba81e5a714a",
+    "url": "/static/media/offwhite wallpaper.d5ef1ff0.jpg"
+  }
+]);
